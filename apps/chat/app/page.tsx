9417007bc046669cb281635955f58ad745271cb4
shayxu <shayxu@tencent.com>
2023-07-11 21:20:34 +0800

    fix: error

--- apps/chat/app/page.tsx
+++ apps/chat/app/page.tsx
@@ -1,7 +1,6 @@
 import { Modal } from "antd";
 import { Home } from "./components/home/home";
-import { AuthPage } from "./components/auth/auth";
-import { useEffect } from "react";
+
 export default async function App() {
   return (
     <>
